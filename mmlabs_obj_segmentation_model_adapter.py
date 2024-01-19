d30ad399bed820d3540e4b45c46cf4a4fb7a56dd
Shadi Mahameed <shadi.m@dataloop.ai>
2024-01-19 14:25:15 +0200

    Updating mmseg codebase

--- mmlabs_obj_segmentation_model_adapter.py
+++ mmlabs_obj_segmentation_model_adapter.py
@@ -1,7 +1,11 @@
 import os
+from mmseg.apis import inference_model, init_model
 
 import dtlpy as dl
 import numpy as np
+import logging
+
+logger = logging.getLogger('MMSegmentation')
 
 
 @dl.Package.decorators.module(description='Model Adapter for mmlabs object segmentation',
@@ -9,17 +13,19 @@ import numpy as np
                               init_inputs={'model_entity': dl.Model})
 class Adapter(dl.BaseModelAdapter):
     def load(self, local_path, **kwargs):
-        from mmseg.apis import init_model
-        os.system("mim download mmsegmentation --config pspnet_r50-d8_4xb4-80k_coco-stuff164k-512x512 --dest .")
+        config_file = 'pspnet_r50-d8_4xb4-80k_coco-stuff164k-512x512.py'
+        checkpoint_file = 'pspnet_r50-d8_512x512_4x4_80k_coco-stuff164k_20210707_152034-0e41b2db.pth'
+
+        if not os.path.exists(config_file) or not os.path.exists(checkpoint_file):
+            os.system("mim download mmsegmentation --config pspnet_r50-d8_4xb4-80k_coco-stuff164k-512x512 --dest .")
+
         print("mmsegmentation downloaded successfully")
         with open("./labels.txt", "r") as file:
             self.coco_labels = [line.replace('\n', '') for line in file.readlines()]
-        config_file = './config_dir/pspnet_r50-d8_4xb4-80k_coco-stuff164k-512x512.py'
-        checkpoint_file = 'pspnet_r50-d8_512x512_4x4_80k_coco-stuff164k_20210707_152034-0e41b2db.pth'
+
         self.model = init_model(config_file, checkpoint_file, device='cpu')
 
     def predict(self, batch, **kwargs):
-        from mmseg.apis import inference_model
         batch_annotations = list()
         for image in batch:
             image_annotations = dl.AnnotationCollection()
@@ -33,6 +39,15 @@ class Adapter(dl.BaseModelAdapter):
                     for columns_i in range(columns_num):
                         row.append(1 if int(pred_row[columns_i]) == id else 0)
                     mask.append(row)
-                image_annotations.add(annotation_definition=dl.Segmentation(geo=np.array(mask), label=self.coco_labels[id]))
-            batch_annotations.append(image_annotations)
+                image_annotations.add(
+                    annotation_definition=dl.Segmentation(geo=np.array(mask),
+                                                          label=self.coco_labels[id]))
+                batch_annotations.append(image_annotations)
         return batch_annotations
+
+
+if __name__ == '__main__':
+    model = dl.models.get(model_id='65a9723545ad6d4b305a8546')
+    adapter = Adapter(model_entity=model)
+    item = dl.items.get(item_id='659dad958625651d89ca8e25')
+    adapter.predict_items(items=[item])
